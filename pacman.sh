e0ee81e284b3a06ae4f44ee4e44e644ec7964441
MightyElemental <12786983+MightyElemental@users.noreply.github.com>
2024-02-10 15:34:56 +0000

    Added basic pacman and yay scripts

---
+++ pacman.sh
@@ -0,0 +1,46 @@
+
+
+pacman -Sy --noconfirm
+
+# Standard
+pacman -S --noconfirm --needed \
+    git \
+    base-devel \
+    breeze-gtk \
+    firefox \
+    thunderbird \
+    code \
+    steam \
+    audacity \
+    gimp \
+    cuda \
+    cuda-tools \
+    openmpi \
+    vlc \
+    catfish \
+    dbeaver \
+    sqlitebrowser \
+    sudo \
+    wget \
+    curl
+
+
+# Latex
+pacman -S --noconfirm --needed \
+    texlive \
+    biber \
+    texstudio 
+
+# Java
+pacman -S --noconfirm --needed \
+    jdk17-openjdk \
+    openjdk17-doc \
+    openjdk17-src \
+    jdk11-openjdk \
+    openjdk11-doc \
+    openjdk11-src \
+    jdk8-openjdk \
+    openjdk8-doc \
+    openjdk8-src \
+    eclipse-java
+
---
+++ yay.sh
@@ -0,0 +1,57 @@
+
+# Install yay if needed
+if ! pacman -Qi yay > /dev/null ; then
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+    cd ..
+    rm -r yay
+fi
+
+# Install AUR packages
+
+yay -S --noconfirm --needed \
+    papirus-maia-icon-theme-git \
+    matcha-gtk-theme \
+    soundux \
+    plexamp-appimage \
+    onedriver-git \
+    k4dirstat \
+    joplin-appimage \
+    archlinux-java-run \
+
+# Games
+yay -S --noconfirm --needed \
+    mangohud-common-git \
+    osu-lazer-bin \
+    betacraft-launcher-bin \
+    minecraft-technic-launcher \
+    minecraft-launcher \
+    nbtexplorer-bin \
+
+# Development
+yay -S --noconfirm --needed \
+    anaconda \
+    android-studio \
+    xilinx-ise \
+    megit \
+    net-logo \
+
+# Creative
+yay -S --noconfirm --needed \
+    gimp-plugin-registry \
+    cura-modern-appimage \
+    drawio-desktop-bin \
+
+# Tools
+yay -S --noconfirm --needed \
+    spectre-meltdown-checker \
+    gtkhash-thunar \
+    qbittorrent \
+    freefilesync-bin \
+    exif \
+    balena-etcher-appimage \
+    mission-center-git \
+    makemkv \
+    x2goclient \
+    downgrade \
