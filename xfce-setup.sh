364d001c891feebf6fdbbd1207af713851803b43
MightyElemental <12786983+MightyElemental@users.noreply.github.com>
2024-12-10 22:15:41 +0000

    Added keyboard shortcut settings

--- xfce-setup.sh
+++ xfce-setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Themes
 xfconf-query -c xsettings -p /Net/IconThemeName -s "Papirus-Dark"
 xfconf-query -c xsettings -p /Net/ThemeName -s "Matcha-dark-azul"
+
+# Shortcuts
+xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Shift><Super>s" -t "string" -s "xfce4-screenshooter -r" -n # Screenshot
+xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Primary><Alt>t" -t "string" -s "xfce4-terminal" -n # Terminal
