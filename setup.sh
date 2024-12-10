6d521069d63e7ae55fdd201884883bbb0cfa856f
MightyElemental <12786983+MightyElemental@users.noreply.github.com>
2024-12-10 21:48:09 +0000

    Added vscode settings

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@
 /bin/bash ./yay.sh
 /bin/bash ./xfce-setup.sh
 /bin/bash ./other-software.sh
+/bin/bash ./vscode-setup.sh
---
+++ vscode-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Tools
+code --install-extension mads-hartmann.bash-ide-vscode # Bash IDE
+code --install-extension DanielSanMedium.dscodegpt # CodeGPT
+code --install-extension eamodio.gitlens # Git Lens
+code --install-extension mhutchie.git-graph # Git Graph
+code --install-extension esbenp.prettier-vscode # Prettier VSCode
+code --install-extension vscode-icons-team.vscode-icons # VSCode Icons
+
+# Languages
+code --install-extension ms-python.python # Python
+code --install-extension ms-python.debugpy # Python Debug
+code --install-extension ms-python.anaconda-extension-pack # Anaconda
+code --install-extension ms-python.pylint # Python Linter
+code --install-extension ms-toolsai.jupyter # Python Jupyter
+code --install-extension ms-pyright.pyright # Python Static Type Checker
+code --install-extension haskell.haskell # Haskell
+code --install-extension justusadam.language-haskell # Haskell Syntax Highlighting
+code --install-extension redhat.java # Java
+code --install-extension redhat.vscode-yaml # YAML
+code --install-extension redhat.vscode-xml # XML
