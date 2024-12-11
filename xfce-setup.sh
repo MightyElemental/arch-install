23b8956babf0ebde2158993935d2ce64f4e4c1a9
MightyElemental <12786983+MightyElemental@users.noreply.github.com>
2024-12-11 14:45:20 +0000

    Corrected xfce settings

--- xfce-setup.sh
+++ xfce-setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Themes
-xfconf-query -c xsettings -p /Net/IconThemeName -s "Papirus-Dark"
-xfconf-query -c xsettings -p /Net/ThemeName -s "Matcha-dark-azul"
+xfconf-query -c xsettings -p /Net/IconThemeName -t "string" -s "Papirus-Dark-Maia" -n
+xfconf-query -c xsettings -p /Net/ThemeName -t "string" -s "Matcha-dark-azul" -n
 
 # Shortcuts
-xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Shift><Super>s" -t "string" -s "xfce4-screenshooter -r" -n # Screenshot
+xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Shift><Super>s" -t "string" -s "xfce4-screenshooter -rc" -n # Screenshot
 xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Primary><Alt>t" -t "string" -s "xfce4-terminal" -n # Terminal
