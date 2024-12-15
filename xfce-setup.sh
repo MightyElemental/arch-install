4f6afe351cf546ec42ca45410693817e46ff0b2b
MightyElemental <12786983+MightyElemental@users.noreply.github.com>
2024-12-15 00:01:11 +0000

    Added startup files

--- xfce-setup.sh
+++ xfce-setup.sh
@@ -7,3 +7,7 @@ xfconf-query -c xsettings -p /Net/ThemeName -t "string" -s "Matcha-dark-azul" -n
 # Shortcuts
 xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Shift><Super>s" -t "string" -s "xfce4-screenshooter -rc" -n # Screenshot
 xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Primary><Alt>t" -t "string" -s "xfce4-terminal" -n # Terminal
+
+# Startup
+mkdir -p ~/.config/autostart
+cp -r ./startup/* ~/.config/autostart
